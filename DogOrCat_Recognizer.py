cd0432a81ced201538052b3d39c38e76ec5ae4b4
arzeyyy <118080823+arzeyyy@users.noreply.github.com>
2022-11-12 23:27:14 +0100

    Update DogOrCat_Recognizer.py

--- DogOrCat_Recognizer.py
+++ DogOrCat_Recognizer.py
@@ -2,7 +2,6 @@ from gc import callbacks
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from keras import layers, models
-from keras.models import load_model
 import numpy as np
 import os
 import cv2
